dc28973a85e571c20682d3ab08267c4a5c92c479
nixxtreme <nixxtreme@msn.com>
2024-09-19 18:31:17 -0600

    Se agregan cambios para URL dinámicas Clase 63

--- gateway/src/main/java/com/micro/gateway/beans/GatewayBeans.java
+++ gateway/src/main/java/com/micro/gateway/beans/GatewayBeans.java
@@ -4,11 +4,13 @@ import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class GatewayBeans {
     @Bean
-    public RouteLocator routeLocator(RouteLocatorBuilder builder) {
+    @Profile(value="eureka-off")
+    public RouteLocator routeLocatorEurekaOff(RouteLocatorBuilder builder) {
         return builder
                 .routes()
                 .route(route->route
@@ -18,6 +20,22 @@ public class GatewayBeans {
                 .route(route->route
                         .path("/report-ms/report/*")
                         .uri("http://localhost:7070")
+                )
+                .build();
+    }
+
+    @Bean
+    @Profile(value="eureka-on")
+    public RouteLocator routeLocatorEurekaOn(RouteLocatorBuilder builder) {
+        return builder
+                .routes()
+                .route(route->route
+                        .path("/micro-crud/company/**")
+                        .uri("lb://micro-crud")
+                )
+                .route(route->route
+                        .path("/report-ms/report/**")
+                        .uri("lb://report-ms")
                 )
                 .build();
     }
