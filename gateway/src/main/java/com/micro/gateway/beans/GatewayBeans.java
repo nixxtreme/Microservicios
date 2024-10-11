4f4176fccf16711792f27a81f70694d9b729ab97
nixxtreme <nixxtreme@msn.com>
2024-10-10 23:35:18 -0600

    Clase 113

--- gateway/src/main/java/com/micro/gateway/beans/GatewayBeans.java
+++ gateway/src/main/java/com/micro/gateway/beans/GatewayBeans.java
@@ -1,5 +1,7 @@
 package com.micro.gateway.beans;
 
+import com.micro.gateway.filters.AuthFilter;
+import lombok.AllArgsConstructor;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -9,7 +11,9 @@ import org.springframework.context.annotation.Profile;
 import java.util.Set;
 
 @Configuration
+@AllArgsConstructor
 public class GatewayBeans {
+    private final AuthFilter authFilter;
     @Bean
     @Profile(value="eureka-off")
     public RouteLocator routeLocatorEurekaOff(RouteLocatorBuilder builder) {
@@ -60,6 +64,35 @@ public class GatewayBeans {
                 )
                 .route(route->route
                         .path("/report-ms/report/**")
+                        .uri("lb://report-ms")
+                )
+                .route(route->route
+                        .path("/micro-crud-fallback/company/**")
+                        .uri("lb://micro-crud-fallback")
+                )
+                .build();
+    }
+
+    @Bean
+    @Profile(value="oauth2")
+    public RouteLocator routeLocatorOauth2(RouteLocatorBuilder builder) {
+        return builder
+                .routes()
+                .route(route->route
+                        .path("/micro-crud/company/**")
+                        .filters(filter -> {
+                            filter.circuitBreaker(config -> config
+                                    .setName("gateway-cb")
+                                    .setStatusCodes(Set.of("400", "500"))
+                                    .setFallbackUri("forward:/micro-crud-fallback/company/*"));
+                            filter.filter(this.authFilter);
+                            return filter;
+                        })
+                        .uri("lb://micro-crud")
+                )
+                .route(route->route
+                        .path("/report-ms/report/**")
+                        .filters(filter -> filter.filter(this.authFilter))
                         .uri("lb://report-ms")
                 )
                 .route(route->route
---
+++ gateway/src/main/java/com/micro/gateway/dto/TokenDto.java
@@ -0,0 +1,15 @@
+package com.micro.gateway.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class TokenDto {
+
+    private String accesstoken;
+
+
+}
